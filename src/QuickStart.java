80ca7d298f5f4c214224a5d58414b8fb2f29c1ae
kishan870 <narasimhakishan392@gmail.com>
2024-07-29 23:25:40 -0400

    quickstart

---
+++ src/QuickStart.java
@@ -0,0 +1,6 @@
+public class QuickStart {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("The Java program is running correctly");
+    }
+}

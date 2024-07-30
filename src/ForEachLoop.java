d19605a88139a18b67a5feff0699df00ccbf5191
kishan870 <narasimhakishan392@gmail.com>
2024-07-30 18:04:06 -0400

    ForEachLoop

---
+++ src/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String args[]) {
+        String[] fruits = {"apple", "orange", "cherry"};
+
+        for(String i: fruits) {
+            System.out.println(i);
+        }
+    }
+}

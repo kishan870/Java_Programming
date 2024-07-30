7b656e1b0ed9c24a15c88390a34de7ebffd0c7df
kishan870 <narasimhakishan392@gmail.com>
2024-07-30 01:39:43 -0400

    printVariables

---
+++ src/PrintVariables.java
@@ -0,0 +1,12 @@
+public class PrintVariables {
+    public static void main(String args[]) {
+        int x = 5, y = 7;
+        String firstName = "Java";
+        String lastName = "Programming";
+
+        System.out.println(x + y);
+
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}

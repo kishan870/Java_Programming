c68e2eb0370df9cfb321492152f93e9e606e1307
kishan870 <narasimhakishan392@gmail.com>
2024-09-06 14:08:58 -0400

    Factorial using recursion

---
+++ src/FactorialUsingResursion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FactorialUsingResursion {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int x = scanner.nextInt();
+
+        System.out.printf("Factorial of %d is %d%n", x, getFactorial(x));
+        
+    }
+
+    public static long getFactorial(int x) {
+
+        if(x == 0)
+            return 1;
+
+        return x * getFactorial(x-1);
+    }
+}
